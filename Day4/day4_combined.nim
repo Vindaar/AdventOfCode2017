2aa25ad2cf17554d6f30566645900520f26a96d7
Vindaar <basti90@gmail.com>
2017-12-04 09:32:25 +0100

    clean up code using mapIt

--- Day4/day4_combined.nim
+++ Day4/day4_combined.nim
@@ -6,11 +6,11 @@ proc hash_and_compare(words: seq[string]): bool =
 
 proc check_password(pword: string, part2 = false): bool =
   # proc to check whether a given password is valid
-  let words = map(split(pword, " "), (word: string) -> string => word)
+  let words = mapIt(split(pword, " "), it)
   if part2 == false:
     result = hash_and_compare(words)
   else:
-    let sorted_words = map(words, (word: string) -> string => foldl(sorted(word, system.cmp), a & b, ""))
+    let sorted_words = mapIt(words, foldl(sorted(it, system.cmp), a & b, ""))
     # given sorted words, can now check for different passwords in same
     # way as before. Create hashset of words, check for differences in length
     result = hash_and_compare(sorted_words)
@@ -19,7 +19,7 @@ proc check_lst_of_passwords(pwords: seq[string], part2 = false): int =
   # receives a list of passwords, checks each with check_password
   # and resturns an int of the number of valid passwords
   # define the regex to match the words in the password
-  result = len(filter(pwords, (pword: string) -> bool => check_password(pword, part2)))
+  result = len(filterIt(pwords, check_password(it, part2)))
 
 proc run_tests() =
   # part 1
@@ -45,14 +45,14 @@ proc run_input() =
   const input = "input.txt"
   const data = slurp(input)
 
-  var pwords: seq[string] = @[]
-  for pword in splitLines(data):
-    if len(pword) > 0:
-      pwords.add(pword)
+  let pwords = filterIt(mapIt(splitLines(data), it), len(it) > 0)
 
   echo "Number of total passwords = $#" % $len(pwords)
-  let valid = check_lst_of_passwords(pwords, true)
-  echo "The number of valid passwords is = ", valid
+  let valid1 = check_lst_of_passwords(pwords)
+  echo "(Part 1): The number of valid passwords is = ", valid1
+
+  let valid2 = check_lst_of_passwords(pwords, true)
+  echo "(Part 2): The number of valid passwords is = ", valid2
   
 proc main() =
   run_tests()
